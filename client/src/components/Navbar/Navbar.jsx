239aaa7f1c90402bd0692d4eb0417be3430159d9
Misbah-khanam <misbahkhanam711@gmail.com>
2023-05-07 11:37:09 +0530

    commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,31 @@
-import React, {useEffect}from 'react'
-import {Link, useNavigate } from 'react-router-dom' ;
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/stack_logo.png';
 import search from '../../assets/search.svg'
 import Avatar from '../../components/Avatar/Avatar'
 import Button from '../../components/Button/Button'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import decode from 'jwt-decode'
+
+
+import {NavLink} from 'react-router-dom';
+import Globe from '../../assets/globe-2.png'
+import './Menu.css';
 
 import "./Navbar.css"
 import { setCurrentUser } from '../../actions/currentUser';
+import { useState } from 'react';
 
 const Navbar = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   var user = useSelector((state) => (state.currentUserReducer))
+  
+  const [menuBtn, setMenuBtn] = useState(false)
 
   const handleLogout = () => {
-    dispatch({type: 'LOGOUT'})
+    dispatch({ type: 'LOGOUT' })
     navigate('/')
     dispatch(setCurrentUser(null))
   }
@@ -33,30 +41,74 @@ const Navbar = () => {
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
   }, [user?.token, dispatch]);
 
+  const handleMenu = () => {
+    setMenuBtn(!menuBtn)
+    console.log(menuBtn)
+  }
+
+
 
 
   return (
+    <div>
+      {
+      menuBtn && <div className='left-sidebar-menu'>
+            <nav className='side-nav-menu'>
+                <NavLink to="/" className='side-nav-links-menu' activeClassName='active' onClick={()=>{setMenuBtn(!menuBtn)}}>
+                    <p>Home</p>
+                </NavLink>
+                <div className='side-nav-div-menu'>
+                    <div><p>PUBLIC</p></div>
+                    <NavLink to='/Questions' className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}} activeClassNmae='active' style={{paddingLeft:"40px"}}>
+                        <img src = {Globe} alt = "Globe"/>
+                        <p style={{paddingLeft: "10px"}}>Questions</p>
+                    </NavLink>
+                    <NavLink to="/Tags" className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}} activeClassNmae='active' style={{paddingLeft:"40px"}}>
+                        <p>Tags</p>
+                    </NavLink>
+                    <NavLink to="/Users" className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}} activeClassNmae='active' style={{paddingLeft:"40px"}}>
+                        <p>Users</p>
+                    </NavLink>
+                    <div></div>
+                    <Link to="/" className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}}  style={{paddingLeft:"40px"}}>
+                        <p>About</p>
+                    </Link>
+                    <Link to="/" className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}}  style={{paddingLeft:"40px"}}>
+                        <p>Products</p>
+                    </Link>
+                    <Link to="/" className='side-nav-links-menu' onClick={()=>{setMenuBtn(!menuBtn)}}  style={{paddingLeft:"40px"}}>
+                        <p>For Teams</p>
+                    </Link>
+                </div>
+            </nav>
+        </div>
+      }
     <nav className='main-nav'>
       <div className='navbar'>
-        <Link to="/" className='nav-item nav-logo'>
-          <img  src={logo} alt= "logo"/>
+        <Link to="/" className='nav-logo'>
+          <img src={logo} alt="logo" />
         </Link>
-        <Link to="/" className='nav-item nav-btn' >About</Link>
-        <Link to="/" className='nav-item nav-btn' >Products</Link>
-        <Link to="/" className='nav-item nav-btn' >For Teams</Link>
+        <div className='icon-bars' onClick={handleMenu}><span className='fas fa-bars'></span></div>
+        <div className='icon-cancel'><span className='fas fa-times'></span></div>
+        <div className='nav-btns'>
+          <li><Link to="/" className='nav-item nav-btn' >About</Link></li>
+          <li><Link to="/" className='nav-item nav-btn' >Products</Link></li>
+          <li><Link to="/" className='nav-item nav-btn' >For Teams</Link></li>
+        </div>
         <form>
-          <input type="text" placeholder='Search'/>
-          <img src={search} alt="search" className='search-icon'/>
+          <input type="text" placeholder='Search' />
+          <img src={search} alt="search" className='search-icon' />
         </form>
-        {user == null ? 
-            <Link to="/Auth" className='nav-item nav-links'>Log in</Link>:
-            <>
-                <Avatar backgroundColor='#009dff' px="10px" py="7px" borderRadius="50%" color="white" ><Link to={`/Users/${user.result._id}`} style={{color:"white" ,textDecoration:'none'}}>{user.result.name.charAt(0).toUpperCase()}</Link></Avatar>
-                <button className='nav-item nav-links' onClick={handleLogout}>Log out</button>
-            </>
+        {user == null ?
+          <Link to="/Auth" className='nav-item nav-links'>Log in</Link> :
+          <>
+            <Avatar backgroundColor='#009dff' px="10px" py="7px" borderRadius="50%" color="white" ><Link to={`/Users/${user.result._id}`} style={{ color: "white", textDecoration: 'none' }}>{user.result.name.charAt(0).toUpperCase()}</Link></Avatar>
+            <button className='nav-item nav-links' onClick={handleLogout}>Log out</button>
+          </>
         }
       </div>
     </nav>
+    </div>
   )
 }
 
--- client/src/components/RightSideBar/Widget.jsx
+++ client/src/components/RightSideBar/Widget.jsx
@@ -10,41 +10,41 @@ const Widget = () => {
             <div className='right-sidebar-div-1'>
                 <div className='right-sidebar-div-2'>
                     <img src={pen} alt="pen" width="18" />
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
                 <div className='right-sidebar-div-2'>
                     <img src={pen} alt="pen" width="18" />
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
             </div>
             <h4>Featured on Meta</h4>
             <div className='right-sidebar-div-1'>
                 <div className='right-sidebar-div-2'>
                     <img src={comment} alt="pen" width="18" />
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
                 <div className='right-sidebar-div-2'>
                     <img src={comment} alt="pen" width="18" />
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
                 <div className='right-sidebar-div-2'>
                     <img src={blackLogo} alt="pen" width="18" />
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
             </div>
             <h4>Hot Meta Posts</h4>
             <div className='right-sidebar-div-1'>
                 <div className='right-sidebar-div-2'>
                     <p>38</p>
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
                 <div className='right-sidebar-div-2'>
                     <p>20</p>
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
                 <div className='right-sidebar-div-2'>
                     <p>14</p>
-                    <p>Observability is key to the future of<br /> software (and your devOPs career)</p>
+                    <p>Observability is key to the future of software (and your devOPs career)</p>
                 </div>
             </div>
         </div>
